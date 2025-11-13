91e40f00f27b178c900f0351b434e909eab6842e
httpsura <lohtupoy228das@gmail.com>
2025-11-13 10:27:39 +0300

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,25 @@
+const booleanValue = false;
+const numberValue = 20.20;
+const stringValue = "30";
+
+console.log("исходные значения");
+ console.log(booleanValue);
+ console.log(numberValue);
+ console.log(stringValue);
+console.log("их типы данных");
+ console.log(typeof booleanValue);
+ console.log(typeof numberValue);
+ console.log(typeof stringValue);
+
+ let booleanValue1 = booleanValue.toString();
+ let numberValue1 = Math.floor(numberValue);
+ let stringValue1 = Number(stringValue);
+ 
+console.log("измененные значения");
+ console.log(booleanValue1);
+ console.log(numberValue1);
+ console.log(stringValue1);
+console.log("их типы данных");
+ console.log(typeof booleanValue1);
+ console.log(typeof numberValue1);
+ console.log(typeof stringValue1);
