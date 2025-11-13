583f9593a41de1976a9f6d0fe0d9d12d1f5f5a0a
AutoUser <autoUser@IKNWNTHEPASSWORD.pythonanywhere.com>
2025-11-13 10:19:15 +0300

    first commit

---
+++ task3.js
@@ -0,0 +1,18 @@
+let myBirthDay = "23.05.2007"
+let myName = "insaf"
+let myAge = 18
+let work = true
+
+console.log(typeof myBirthDay)
+console.log(typeof myName)
+console.log(typeof myAge)
+console.log(typeof work)
+
+console.log(myBirthDay)
+console.log(myName)
+console.log(myAge)
+console.log(work)
+
+myAge = 19
+console.log(myAge)
+
